3920b30d7ad4022cc6532d49bc9397196bea4778
t-boeck <thomas.p.boeck@gmail.com>
2025-05-07 13:57:08 -0700

    add favicon to translate page

--- aldus/app.py
+++ aldus/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 def generate_translation(eng_paragraphs, model):
     yield "<html><head><meta charset='utf-8'><title>Translation Progress</title>"
+    yield "<link rel='icon' type='image/png' href='/static/favicon.png'>"
     yield """<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
       body { background-color: #f8f9fa; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; }
